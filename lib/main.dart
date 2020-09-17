fb7449ec5c200c4679cff016a2075e2b805634fe
Robiya160198 <robiya.farmonova.98@mail.ru>
2020-09-17 22:12:50 +0500

    "salom qaleysiz z"

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dilshod+Robiya=Sevgi',
       theme: ThemeData(
         // This is the theme of your application.
         //
